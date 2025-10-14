ef6c2302eef241fc34be99ff2cb1339556884127
asahamada20 <164310674+asahamada20@users.noreply.github.com>
2025-10-14 23:14:16 +0700

    Update code-example.py

--- praktikum/week2-cryptosystem/src/code-example.py
+++ praktikum/week2-cryptosystem/src/code-example.py
@@ -1,4 +1,3 @@
-'''
 def encrypt(plaintext, key):
     result = ""
     for char in plaintext:
@@ -29,4 +28,5 @@ if __name__ == "__main__":
     print("Plaintext :", message)
     print("Ciphertext:", enc)
     print("Decrypted :", dec)
-'''
+
+
